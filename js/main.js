7fd773e20e723e9b11971b76b11e6c67bb56bbe4
dltkdals20 <sangming2007@naver.com>
2022-10-20 21:46:43 +0900

    세번째 파일

--- js/main.js
+++ js/main.js
@@ -3,5 +3,24 @@ $(function () {
         arrows: false,
         dots: true,
 
+    });
+    $('.top_banner i').on('click', function () {
+        $('.top_banner').slideUp(200)
+    })
+
+    $(window).on('scroll', function () {
+        var sct = $(window).scrollTop()
+        if (sct > 0) {
+            $('.header .bottom').addClass('on')
+        } else {
+            $('.header .bottom').removeClass('on')
+        }
+    })
+    $('.header .grobal').on('click', function () {
+        $('.header .link').toggleClass('on')
     })
 })
+
+
+
+
