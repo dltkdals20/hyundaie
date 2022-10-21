6eba1fbd35264d8eed4f1231101029ed0a04e2fe
dltkdals20 <sangming2007@naver.com>
2022-10-21 21:51:42 +0900

    내전째

--- js/main.js
+++ js/main.js
@@ -5,7 +5,8 @@ $(function () {
 
     });
     $('.top_banner i').on('click', function () {
-        $('.top_banner').slideUp(200)
+        $('.top_banner').slideUp(200);
+        $('.main').addClass('on')
     })
 
     $(window).on('scroll', function () {
@@ -18,7 +19,33 @@ $(function () {
     })
     $('.header .grobal').on('click', function () {
         $('.header .link').toggleClass('on')
+    });
+
+    $('.main_slide').slick({
+        arrows: false,
+        autoplay: true,
+        pauseOnHover: false,
+    });
+
+    $('.main_slide').on('afterChange', function (e, s, c) {
+        $('.main_visual .page li').eq(c).addClass('on').siblings().removeClass('on')
+        $('.num strong').text("0" + (c + 1))
+    });
+
+    $('.page li').on('click', function () {
+        var idx = $(this).index()
+        $('.main_slide').slick('slickGoTo', idx)
     })
+
+
+
+    $('.main_visual .arrows i:first-child').on('click', function () {
+        $('.main_slide').slick('slickPrev')
+    })
+    $('.main_visual .arrows i:last-child').on('click', function () {
+        $('.main_slide').slick('slickNext')
+    });
+
 })
 
 
